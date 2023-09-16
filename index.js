54aa8a0f478e86ada8e31add346800b6579afd91
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-09-16 14:29:16 +0500

    updated schemas

--- index.js
+++ index.js
@@ -38,6 +38,23 @@ const userSchema = new mongoose.Schema({
 const Gameuser = new mongoose.model("Gameuser", userSchema);
 
 
+const userSchema2 = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  creditCardInformation: String
+});
+const Gameuser2 = new mongoose.model("Gameuser2", userSchema2);
+
+
+const userSchema555 = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+const Gameuser555 = new mongoose.model("Gameuser555", userSchema555);
+
+
 const cakeSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Gameuser' },
   title: String,
@@ -45,6 +62,139 @@ const cakeSchema = new mongoose.Schema({
   description: String,
 });
 const Cake = mongoose.model("Cake", cakeSchema);
+
+
+
+// Define the League schema
+const leagueSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  // Other league fields...
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Gameuser2' }],
+});
+
+const League = mongoose.model('League', leagueSchema);
+
+app.post('/api/create-league', async (req, res) => {
+  const { name, description } = req.body;
+
+  try {
+      const newLeague = new League({
+          name: name,
+          description: description,
+          users: [], // Initialize with an empty array of users
+      });
+
+      await newLeague.save();
+
+      res.status(201).json({ message: 'League created successfully', league: newLeague });
+  } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Route to fetch all leagues
+app.get('/api/get-leagues', async (req, res) => {
+  try {
+      const leagues = await League.find({});
+      res.json({ leagues });
+  } catch (error) {
+      res.status(500).json({ message: 'Error fetching leagues' });
+  }
+});
+
+const userLeagueSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Gameuser2' },
+  leagueId: { type: mongoose.Schema.Types.ObjectId, ref: 'League' },
+});
+
+const UserLeague = mongoose.model('UserLeague', userLeagueSchema);
+
+app.post('/api/join-league/:userId/:leagueId', async (req, res) => {
+  const { userId, leagueId } = req.params;
+
+  try {
+      // Create a new UserLeague document to represent the user's league membership
+      const userLeague = new UserLeague({
+          userId: userId,
+          leagueId: leagueId,
+      });
+      await userLeague.save();
+
+      // Update the League document to add the user to the 'users' array
+      await League.findByIdAndUpdate(leagueId, { $push: { users: userId } });
+
+      res.status(200).json({ message: 'User joined the league successfully' });
+  } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+app.get('/api/check-join-status/:userId/:leagueId', async (req, res) => {
+  const { userId, leagueId } = req.params;
+
+  try {
+      const userLeague = await UserLeague.findOne({ userId: userId, leagueId: leagueId });
+
+      if (userLeague) {
+          res.status(200).json({ joined: true });
+      } else {
+          res.status(200).json({ joined: false });
+      }
+  } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Import required modules and set up your Express app
+
+// Assuming you already have a League and UserLeague model
+
+// Define a new route to get users who have joined a specific league
+app.get('/api/get-league-users/:leagueId', async (req, res) => {
+  const { leagueId } = req.params;
+
+  try {
+    // Find the league by its ID
+    const league = await League.findById(leagueId);
+
+    if (!league) {
+      return res.status(404).json({ message: 'League not found' });
+    }
+
+    // Find user leagues associated with this league
+    const userLeagues = await UserLeague.find({ leagueId: leagueId });
+
+    if (!userLeagues || userLeagues.length === 0) {
+      return res.status(200).json({ message: 'No users have joined this league', users: [] });
+    }
+
+    // Extract user IDs from user leagues
+    const userIds = userLeagues.map(userLeague => userLeague.userId);
+
+    // Find users by their IDs
+    const users = await Gameuser2.find({ _id: { $in: userIds } });
+
+    if (!users || users.length === 0) {
+      return res.status(200).json({ message: 'No users found for this league', users: [] });
+    }
+
+    // Respond with the list of users who have joined this league
+    res.status(200).json({ message: 'Users who have joined this league', users });
+  } catch (error) {
+    console.error('Error fetching league users:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+
+
+
+
+
+
+
+
 app.post('/users/:objectId/cakes', async (req, res) => {
   const { objectId } = req.params;
   const { title, date, description } = req.body;
@@ -164,9 +314,124 @@ app.get('/api/get-users', async (req, res) => {
   }
 });
 
-app.post("/submit-order", (req, res) => {
+
+
+//Routes for game users
+
+//Routes
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Gameuser2.findOne({ email: email });
+
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) {
+        const objectId = user._id.toString();
+        res.status(200).json({ message: 'Login successful', objectId: objectId });
+      } else {
+        res.status(401).json({ message: 'Invalid password' });
+      }
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.post('/api/register', async (req, res) => {
+  const { name, email, password , creditCardInformation} = req.body;
+
+  try {
+    const existingUser = await Gameuser2.findOne({ email: email });
+
+    if (existingUser) {
+      res.status(409).json({ message: 'User already exists' });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+
+      const newUser = new Gameuser2({
+        name: name,
+        email: email,
+        password: hashedPassword,
+        creditCardInformation:creditCardInformation, 
+      });
+
+      await newUser.save();
+      res.status(200).json({ message: 'Registration successful', userName: name , userEmail: email });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/api/users/:objectId', async (req, res) => {
+  const { objectId } = req.params;
+
+  try {
+    const user = await Gameuser2.findById(objectId);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// API endpoint to fetch all user data
+app.get('/api/get-game-users', async (req, res) => {
+  try {
+    const users = await Gameuser2.find({});
+    res.json({ users });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching user data' });
+  }
+});
+
+//Routes for games users end here
+
+
+//Routes for game users
+
+//Routes
+app.post('/admin/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Gameuser555.findOne({ email: email });
+
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (passwordMatch) {
+        const objectId = user._id.toString();
+        res.status(200).json({ message: 'Login successful', objectId: objectId });
+      } else {
+        res.status(401).json({ message: 'Invalid password' });
+      }
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+/// end for admin
+
+
+
+
+app.post("/submit-welcome-note", (req, res) => {
   // Extract the form data from the request body
-  const { itemName, userName, userAddress , userEmail , phone , shipping , tshirtType , quantity , tshirtSize , imageUrl} = req.body;
+  const {  userName , userEmail} = req.body;
+  console.log(userEmail);
+  console.log(userName);
+  console.log("test");
 
   // Send an email with the form details using Nodemailer or your preferred email library
   // Here's an example using Nodemailer
@@ -187,42 +452,28 @@ const transporter = nodemailer.createTransport({
 const storeMailOptions = {
   from: userEmail,
   to: "vascularbundle43@gmail.com",
-  subject: "New T-shirt Order",
+  subject: "Fantasy MMAdness",
   html: `
-    <h2 style="color: #ff523b;">New T-shirt Order</h2>
+    <h2 style="color: #ff523b;">Fantasy MMAdness</h2>
     <hr style="border: 1px solid #ccc;">
-    <img src="${imageUrl}" alt="Product Image" style="max-width: 250px; padding:10px; background-color:#f5f1ee;margin:15px 0;">
-    <p><strong>Item:</strong> ${itemName}</p>
-    <p><strong>User Name:</strong> ${userName}</p>
-    <p><strong>User Address:</strong> ${userAddress}</p>
-    <p><strong>User Phone Number:</strong> ${phone}</p>
-    <p><strong>User Email:</strong> ${userEmail}</p>
-    <p><strong>Shipping:</strong> ${shipping}</p>
-    <p><strong>T-shirt Type:</strong> ${tshirtType}</p>
-    <p><strong>T-shirt Size:</strong> ${tshirtSize}</p>
-    <p><strong>Quantity:</strong> ${quantity}</p>
+    <p>Another user added</p>
   `,
 };
 
 const userMailOptions = {
   from: "wajih786hassan@gmail.com",
   to: userEmail,
-  subject: "Thank you for placing the order",
+  subject: "Fantasy MMAdness",
   html: `
-    <h2 style="color: #00a86b;">Thank you for placing the order</h2>
-    <p>Hello ${userName},</p>
-    <p>We appreciate your order and will begin processing it soon.</p>
-    <h3>Your Order Details:</h3>
-    <img src="${imageUrl}" alt="Product Image" style="max-width: 250px; padding:10px; background-color:#f5f1ee;margin:15px 0;">
-    <p><strong>Ordered Item:</strong> ${itemName}</p>
-    <p><strong>Your Address:</strong> ${userAddress}</p>
-    <p><strong>Phone Number:</strong> ${phone}</p>
-    <p><strong>Shipping:</strong> ${shipping}</p>
-    <p><strong>T-shirt Type:</strong> ${tshirtType}</p>
-    <p><strong>T-shirt Size:</strong> ${tshirtSize}</p>
-    <p><strong>Quantity:</strong> ${quantity}</p>
-    <p>If you have any questions or concerns, feel free to contact us.</p>
-    <p>Thank you again for choosing our store!</p>
+  <h2 style="color: red;">Welcome to Fantasy MMAdness</h2>
+  <p>Hello ${userName},</p>
+  <p>Thank you for signing up for Fantasy MMAdness! We're excited to have you on board and join our community of MMA enthusiasts.</p>
+  <p>With Fantasy MMAdness, you can create and manage your own fantasy MMA leagues, make predictions, and compete with others to prove your MMA knowledge.</p>
+  <p>Your journey into the world of MMA fantasy gaming begins now. Get ready for some adrenaline-pumping action and fierce competition!</p>
+  <p>If you have any questions or need assistance, don't hesitate to reach out to our support team at support@fantasymmadness.com.</p>
+  <p>Once again, welcome to Fantasy MMAdness, ${userName}! Let the battles begin!</p>
+  <p>Best regards,</p>
+  <p>The Fantasy MMAdness Team</p> 
   `,
 };
 
@@ -429,6 +680,23 @@ app.post('/api/scores', async (req, res) => {
     res.status(400).send(error);
   }
 });
+app.delete('/api/scores/:playerName', async (req, res) => {
+  const playerName = req.params.playerName;
+
+  try {
+    const result = await Score.deleteMany({ playerName });
+
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: `Deleted ${result.deletedCount} records for ${playerName}` });
+    } else {
+      res.status(404).json({ message: `No records found for ${playerName}` });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 
 // API endpoint to retrieve scores
 app.get('/api/scores', async (req, res) => {
@@ -462,6 +730,106 @@ app.delete('/api/scores/:id', async (req, res) => {
   }
 });
 
+
+
+
+
+
+
+// for mma
+
+const scoreSchemamma = new mongoose.Schema({
+  playerName: String,
+  playerRound: Number,
+  stPrediction1: Number,
+  stPrediction2: Number,
+  kiPrediction1: Number,
+  kiPrediction2: Number,
+  knPrediction1: Number,
+  knPrediction2: Number,
+  elPrediction1: Number,
+  elPrediction2: Number,
+  spPrediction1: Number,
+  spPrediction2: Number,
+  rwPrediction1: Number,
+  rwPrediction2: Number,
+});
+
+const ScoreMma = mongoose.model('ScoreMma', scoreSchemamma);
+
+app.delete('/api/mma/scores/:playerName', async (req, res) => {
+  const playerName = req.params.playerName;
+
+  try {
+    const result = await ScoreMma.deleteMany({ playerName });
+
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: `Deleted ${result.deletedCount} records for ${playerName}` });
+    } else {
+      res.status(404).json({ message: `No records found for ${playerName}` });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// API endpoint to create MMA scores
+app.post('/api/mma/scores', async (req, res) => {
+  try {
+    const {
+      playerName,
+      playerRound,
+      stPrediction1,
+      stPrediction2,
+      kiPrediction1,
+      kiPrediction2,
+      knPrediction1,
+      knPrediction2,
+      elPrediction1,
+      elPrediction2,
+      spPrediction1,
+      spPrediction2,
+      rwPrediction1,
+      rwPrediction2,
+    } = req.body;
+
+    const scoreMma = new ScoreMma({
+      playerName,
+      playerRound,
+      stPrediction1,
+      stPrediction2,
+      kiPrediction1,
+      kiPrediction2,
+      knPrediction1,
+      knPrediction2,
+      elPrediction1,
+      elPrediction2,
+      spPrediction1,
+      spPrediction2,
+      rwPrediction1,
+      rwPrediction2,
+    });
+
+    await scoreMma.save();
+    res.status(201).send(scoreMma);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// API endpoint to retrieve MMA scores
+app.get('/api/mma/scores', async (req, res) => {
+  try {
+    const scoresMma = await ScoreMma.find();
+    res.send(scoresMma);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+// for mma ends here
 
 
 
@@ -543,6 +911,35 @@ app.get('/api/fetchRoundResults', async (req, res) => {
   try {
     const results = await RoundResult.findOne({ adminPlayerId }).exec();
     res.json(results);
+  } catch (error) {
+    console.error('Error fetching round results:', error);
+    res.status(500).json({ error: 'Error fetching round results' });
+  }
+});
+app.get('/fetchRoundResults', async (req, res) => {
+  const { adminPlayerId, roundNumber } = req.query;
+
+  try {
+    const results = await RoundResult.findOne({ adminPlayerId }).exec();
+    if (results) {
+      // Modify this part to extract the round results for the specified round
+      const game = results.games.find(game => game.rounds.some(round => round.roundNumber === parseInt(roundNumber)));
+      
+      if (game) {
+        const round = game.rounds.find(round => round.roundNumber === parseInt(roundNumber));
+        
+        if (round) {
+          // Respond with the round results
+          res.json({ gameNumber: game.gameNumber, round: round });
+        } else {
+          res.status(404).json({ error: 'Round not found' });
+        }
+      } else {
+        res.status(404).json({ error: 'Game not found' });
+      }
+    } else {
+      res.status(404).json({ error: 'Admin player not found' });
+    }
   } catch (error) {
     console.error('Error fetching round results:', error);
     res.status(500).json({ error: 'Error fetching round results' });
